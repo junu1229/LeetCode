f92d1bdcce2f71595245351352dd07ebf91f0ec6
junu1229 <133620199+junu1229@users.noreply.github.com>
2023-08-09 13:53:31 +0900

    Time: 5 ms (61.43%), Space: 44.1 MB (19.16%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,47 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
-        int answer = 0;
-        String as;
-        as = s.replaceAll("[L,C,D,M]", "");
-		if (as.contains("IV")) {
-			answer += 4;
-		} else if (as.contains("IX")||as.equals("IX")) {
-			answer += 9;
-		} else {
-			answer += as.replaceAll("[X,I]", "").length()*5;
-			answer += as.replaceAll("[X,V]", "").length();
-		} 
-		as = s.replaceAll("[I,V,D,M]", "");
-		if (as.contains("XL")) {
-			answer += 40;
-		} else if (as.contains("XC")) {
-			answer += 90;
-		} else if(s.contains("IX")){
-			answer += (as.replaceAll("[C,L]", "").length()-1)*10;
-			answer += as.replaceAll("[C,X]", "").length()*50;
-		} else {
-			answer += as.replaceAll("[C,L]", "").length()*10;
-			answer += as.replaceAll("[C,X]", "").length()*50;
-			
-		}
-		as = s.replaceAll("[I,V,X,L]", "");
-		if (as.contains("CD")) {
-			answer += 400;
-		} else if (as.contains("CM")) {
-			answer += 900;
-		} else if(s.contains("XC")){
-			answer += (as.replaceAll("[D,M]", "").length()-1)*100;
-			answer += as.replaceAll("[C,M]", "").length()*500;
-		} else {
-			answer += as.replaceAll("[D,M]", "").length()*100;
-			answer += as.replaceAll("[C,M]", "").length()*500;
-		}
-		as = s.replaceAll("[I,V,X,L,D]", "");
-		if (as.contains("CM")) {
-			answer += (as.replaceAll("C", "").length()-1)*1000;
-		} else {
-			answer += (as.replaceAll("C", "").length())*1000;
-		}
-        return answer;
+        Map<Character, Integer> m = new HashMap<>();
+        
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+        
+        int ans = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ans -= m.get(s.charAt(i));
+            } else {
+                ans += m.get(s.charAt(i));
+            }
+        }
+        
+        return ans;
     }
 }
